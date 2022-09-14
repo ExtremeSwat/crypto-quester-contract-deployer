1e035b705d78b7946bcb07e15c0afb22ec3dbac6
Edward Cristi Butoiu <butoiu.edwardcristi@gmail.com>
2022-09-15 02:11:11 +0300

    initial files

---
+++ contracts/CryptoQuest.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./CryptoQuestDeployer.sol";
+
+abstract contract CryptoQuest is CryptoQuestDeployer {
+    function createUser() public returns (uint256) {
+
+    }
+
+    function createNewChallenge() public returns (uint256) {
+
+    }
+
+    function checkIfChallengeHasBeenComplete() public returns (uint256) {
+        
+    }
+}
