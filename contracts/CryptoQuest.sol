e93a05829076e95d862980735a08b561c7f0b4d2
Edward Cristi Butoiu <butoiu.edwardcristi@gmail.com>
2022-09-15 23:34:51 +0300

    rewrote some of the contract's internal fields due to how tableland's API is working, this should be better, also cleaned up SQLHelpers

--- contracts/CryptoQuest.sol
+++ contracts/CryptoQuest.sol
@@ -4,29 +4,23 @@ pragma solidity ^0.8.17;
 import "./CryptoQuestDeployer.sol";
 
 abstract contract CryptoQuest is CryptoQuestDeployer {
-
     //Events
-    event ChallengeCreated(address indexed _challengeOwner);
+    event ChallengeCreated(address indexed _challengeOwner, string skinName);
 
-    function createNewChallenge() public returns (uint256) {
+    function createNewChallenge() public returns (uint256) {}
 
-    }
-
-    function checkIfChallengeHasBeenComplete() public returns (uint256) {
-        
-    }
-
-
-    function addNewSkin(string memory skinName, string memory ipfsHash) payable {
-        string insertStatement = SQLHelpers.toInsert
-        (
-            mapSkinsTableName, 
-            string.concat (
-                skinName,
-                ipfsHash
-            )
+    function addNewSkin(string memory skinName, string memory ipfsHash) public payable
+    {
+        string memory insertStatement = SQLHelpers.toInsert(
+            mapSkinsTablePrefix,
+            mapSkinsTableId,
+            string.concat(
+                "skinName,",
+                "ipfsHash"
+            ),
+            string.concat(skinName, ipfsHash)
         );
 
-        _tableland.runSQL(insertStatement);
+        _tableland.runSQL(address(this), mapSkinsTableId, insertStatement);
     }
 }
--- contracts/CryptoQuestDeployer.sol
+++ contracts/CryptoQuestDeployer.sol
@@ -9,17 +9,26 @@ import "./SQLHelpers.sol";
 
 contract CryptoQuestDeployer is Ownable, ERC721Holder {
     // base tables
-    string mapSkinsTableName;
-    string challengesTableName;
-    string challengeLocationsTableName;
-    string participantsTableName;
-    string participantProgressTableName;
+    string mapSkinsTablePrefix = "mapSkins";
+    uint256 mapSkinsTableId;
+
+    string challengesPrefix = "challenges";
+    uint256 challengesTableId;
+
+    string challengeLocationsPrefix = "challenge_locations";
+    uint256 challengeLocationsId;
+
+    string participantsPrefix = "participants";
+    uint256 participantsTableId;
+
+    string participantProgressPrefix = "participant_progress";
+    uint256 participantsProgressTableId;
 
     // additional tables
     mapping(string => uint256) customTables;
 
     // Interface to the `TablelandTables` registry contract
-    ITablelandTables private _tableland;
+    ITablelandTables internal _tableland;
 
     constructor(address registry) payable {
         _tableland = ITablelandTables(registry);
@@ -28,58 +37,43 @@ contract CryptoQuestDeployer is Ownable, ERC721Holder {
     }
 
     function initializeBaseTables() public payable onlyOwner {
-        mapSkinsTableName = SQLHelpers.toNameFromId(
-            "mapSkins",
-            _tableland.createTable(
-                address(this),
-                SQLHelpers.toCreateFromSchema(
-                    "mapSkins",
-                    "(id integer primary key not null, skinName text not null, ipfsHash text not null, unique(ipfsHash), unique(skinName))"
-                )
+        mapSkinsTableId = _tableland.createTable(
+            address(this),
+            SQLHelpers.toCreateFromSchema(
+                mapSkinsTablePrefix,
+                "(id integer primary key not null, skinName text not null, ipfsHash text not null, unique(ipfsHash), unique(skinName))"
             )
         );
 
-        challengesTableName = SQLHelpers.toNameFromId(
-            "challenges",
-            _tableland.createTable(
-                address(this),
-                SQLHelpers.toCreateFromSchema(
-                    "challenges",
-                    "(id integer primary key NOT NULL,title text not null unique,description text not null,fromTimestamp integer not null,toTimestamp integer not null,triggerTimestamp integer,owner text not null,creationTimestamp integer not null,mapSkinId integer)"
-                )
+        challengesTableId = _tableland.createTable(
+            address(this),
+            SQLHelpers.toCreateFromSchema(
+                challengesPrefix,
+                "(id integer primary key NOT NULL,title text not null unique,description text not null,fromTimestamp integer not null,toTimestamp integer not null,triggerTimestamp integer,owner text not null,creationTimestamp integer not null,mapSkinId integer)"
             )
         );
 
-        challengeLocationsTableName = SQLHelpers.toNameFromId(
-            "challenge_locations",
-            _tableland.createTable(
-                address(this),
-                SQLHelpers.toCreateFromSchema(
-                    "challenge_locations",
-                    "(id integer not null primary key,hint TEXT,latitude real not null,longitude real not null,creationTimestamp integer not null,challengeId integer not null)"
-                )
+        challengeLocationsId = _tableland.createTable(
+            address(this),
+            SQLHelpers.toCreateFromSchema(
+                challengeLocationsPrefix,
+                "(id integer not null primary key,hint TEXT,latitude real not null,longitude real not null,creationTimestamp integer not null,challengeId integer not null)"
             )
         );
 
-        participantsTableName = SQLHelpers.toNameFromId(
-            "participants",
-            _tableland.createTable(
-                address(this),
-                SQLHelpers.toCreateFromSchema(
-                    "participants",
-                    "(id integer primary key not null, participant_address text not null, join_timestamp integer not null, challengeId integer not null, unique(participant_address, challengeId)"
-                )
+        participantsTableId = _tableland.createTable(
+            address(this),
+            SQLHelpers.toCreateFromSchema(
+                participantsPrefix,
+                "(id integer primary key not null, participant_address text not null, join_timestamp integer not null, challengeId integer not null, unique(participant_address, challengeId)"
             )
         );
 
-        participantProgressTableName = SQLHelpers.toNameFromId(
-            "participant_progress",
-            _tableland.createTable(
-                address(this),
-                SQLHelpers.toCreateFromSchema(
-                    "participant_progress",
-                    "(id integer primary key not null, challenge_participant_id integer not null, challenge_location_id integer not null, challenge_visit_timestamp integer not null)"
-                )
+        participantsProgressTableId = _tableland.createTable(
+            address(this),
+            SQLHelpers.toCreateFromSchema(
+                "participant_progress",
+                "(id integer primary key not null, challenge_participant_id integer not null, challenge_location_id integer not null, challenge_visit_timestamp integer not null, unique(challenge_participant_id, challenge_location_id))"
             )
         );
     }
