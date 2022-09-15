ee0827c27a55af35d8a946878b60f0c09fec1111
Edward Cristi Butoiu <butoiu.edwardcristi@gmail.com>
2022-09-15 22:00:31 +0300

    added sql helper lib & wireup for possible CTOR direct init of initial tables w/ additional custom ones if needed

--- contracts/CryptoQuestDeployer.sol
+++ contracts/CryptoQuestDeployer.sol
@@ -5,22 +5,54 @@ import "@tableland/evm/contracts/ITablelandTables.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "./SQLHelpers.sol";
 
 contract CryptoQuestDeployer is Ownable, ERC721Holder {
-    // A mapping that holds `tableName` and its `tableId`
-    mapping(string => uint256) public tables;
-    // nterface to the `TablelandTables` registry contract
+    // base tables
+    string mapSkinsTableName;
+    string challengesTableName;
+    string challengeLocationsTableName;
+    string participantsTableName;
+    string participantProgressTableName;
+
+    // additional tables
+    mapping(string => uint256) customTables;
+
+    // Interface to the `TablelandTables` registry contract
     ITablelandTables private _tableland;
 
     constructor(address registry) payable {
         _tableland = ITablelandTables(registry);
     }
 
-    receive() external payable {}
+    function initializeBaseTables() public payable onlyOwner {
+        mapSkinsTableName = SQLHelpers.toCreateFromSchema(
+            "mapSkins",
+            "(id integer primary key not null, skinName text not null, ipfsHash text not null)"
+        );
+        challengesTableName = SQLHelpers.toCreateFromSchema(
+            "challenges",
+            "(id integer primary key not null, skinName text not null, ipfsHash text not null)"
+        );
+        challengeLocationsTableName = SQLHelpers.toCreateFromSchema(
+            "challenge_locations",
+            "(id integer primary key not null, skinName text not null, ipfsHash text not null)"
+        );
+        participantsTableName = SQLHelpers.toCreateFromSchema(
+            "participants",
+            "(id integer primary key not null, skinName text not null, ipfsHash text not null)"
+        );
+        participantProgressTableName = SQLHelpers.toCreateFromSchema(
+            "participant_progress",
+            "(id integer primary key not null, skinName text not null, ipfsHash text not null)"
+        );
+    }
 
-    fallback() external payable {}
-
-    function create(string memory prefix, string memory createStatement) onlyOwner() public payable
+    function create(string memory prefix, string memory createStatement)
+        public
+        payable
+        onlyOwner
+        returns (string memory)
     {
         uint256 tableId = _tableland.createTable(
             address(this),
@@ -42,6 +74,12 @@ contract CryptoQuestDeployer is Ownable, ERC721Holder {
             Strings.toString(tableId)
         );
 
-        tables[tableName] = tableId;
+        customTables[tableName] = tableId;
+
+        return tableName;
     }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
